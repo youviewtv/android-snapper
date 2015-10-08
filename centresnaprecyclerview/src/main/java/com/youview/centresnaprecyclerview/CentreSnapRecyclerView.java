62ddbb8947d21a3f51abe91b0b976fbca9262c6d
Chris Bowers <chris.bowers@youview.com>
2015-10-08 18:05:29 +0100

    Add abstract CentreSnapRecyclerView and associated components.
    CentreSnapRecyclerView provides the basis for a horizontal RecyclerView that is centre-aligned (i.e. the 'selected' view is in the centre). The associated CentreSnapScrollListener and CentreScrollingLinearLayoutManager ensure scrolls maintain this property. The SnapRecyclerAdapter allows the adapter to be aware when the 'selected' view has changed.

---
+++ centresnaprecyclerview/src/main/java/com/youview/centresnaprecyclerview/CentreSnapRecyclerView.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright (c) 2015 YouView TV Limited.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.youview.centresnaprecyclerview;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Rect;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * <p>Abstract implementation of a {@link RecyclerView} that provides <i>most</i> of the work for
+ * a centre-aligned and centre-snapping {@code RecyclerView}. Sub-classes must implement
+ * {@link #getChildWidth()} to provide the width of child {@code View}s, as the behaviours
+ * that make centring possible are very measurement-aware.</p>
+ */
+public abstract class CentreSnapRecyclerView extends RecyclerView {
+    CentreScrollingLinearLayoutManager mLayoutManager;
+
+    // State variables
+    private int mMeasuredWidth;
+    private boolean mMeasurementsValid;
+
+    public CentreSnapRecyclerView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CentreSnapRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CentreSnapRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    /**
+     * <p>Sets up initial state of this {@code RecyclerView}.</p>
+     */
+    protected void init() {
+        mLayoutManager = new CentreScrollingLinearLayoutManager(getContext(), HORIZONTAL, false);
+        setLayoutManager(mLayoutManager);
+        addOnScrollListener(new CentreSnapScrollListener());
+
+        ItemDecoration defaultItemDecoration = getDefaultItemDecoration();
+        if (defaultItemDecoration != null) {
+            addItemDecoration(getDefaultItemDecoration());
+        }
+    }
+
+    /**
+     * <p>Implementation-specific method to retrieve the width of each child shown in this
+     * {@link RecyclerView}. Fixed-width works best, but if there is a marginal variance in width,
+     * an average tends to do the trick.</p>
+     *
+     * @return The width (fixed or average) of children of this {@link RecyclerView}.
+     */
+    protected abstract int getChildWidth();
+
+    /**
+     * <p>Provides a {@link RecyclerView.ItemDecoration} that gets added by default to this
+     * {@code CentreSnapRecyclerView}. This base implementation creates empty space at the beginning
+     * and end of the view such that the first and last elements can reach the centre.</p>
+     *
+     * <p>It is safe to return {@code null} from this method if this is not desired.</p>
+     *
+     * @return An {@link RecyclerView.ItemDecoration} that adds empty space to the start and end of
+     *         the {@code RecyclerView} to allow all children to be centred.
+     */
+    @Nullable
+    protected ItemDecoration getDefaultItemDecoration() {
+        return new ItemDecoration() {
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, State state) {
+                int parentWidth = parent.getWidth();
+                int spacingSize = (parentWidth - getChildWidth()) / 2;
+                int position = parent.getChildAdapterPosition(view);
+                if (position == 0) {
+                    outRect.left = spacingSize;
+                } else if (position == parent.getAdapter().getItemCount() - 1) {
+                    outRect.right = spacingSize;
+                }
+            }
+        };
+    }
+
+    /**
+     * <p>Overrides {@link RecyclerView#onMeasure(int, int)} not to change the measurement logic,
+     * but to calculate/retrieve the width of its children, and update other measurement-aware
+     * components.</p>
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onMeasure(int widthSpec, int heightSpec) {
+        super.onMeasure(widthSpec, heightSpec);
+        int measuredWidth = getMeasuredWidth();
+        if (measuredWidth != mMeasuredWidth) {
+            mMeasuredWidth = measuredWidth;
+            onWidthChanged();
+        }
+
+        // Guard code that is potentially expensive.
+        if (!mMeasurementsValid) {
+            mMeasurementsValid = true;
+            int childWidth = getChildWidth();
+            mLayoutManager.setNewMeasurements(mMeasuredWidth, childWidth);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>When a change in layout happens, this {@code RecyclerView} automatically scrolls again to
+     * the current item, to ensure it remains in the centre.</p>
+     * <p><strong>Note:</strong> this means the {@link CentreSnapScrollListener} will detect a
+     * scroll and notify the {@link SnapRecyclerAdapter} accordingly.</p>
+     */
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        if (changed) {
+            SnapRecyclerAdapter adapter = (SnapRecyclerAdapter) getAdapter();
+            scrollToPosition(adapter.getCurrentPosition());
+        }
+    }
+
+    @Override
+    public void scrollToPosition(int position) {
+        // Our scrolling algorithm means all scrolls are smooth.
+        smoothScrollToPosition(position);
+    }
+
+    /**
+     * <p>Responds to a change in dimensions and resets state variables as appropriate.</p>
+     */
+    private void onWidthChanged() {
+        mMeasurementsValid = false;
+        mLayoutManager.invalidateMeasurements();
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>This class must be used with an implementation of {@link SnapRecyclerAdapter}.</p>
+     */
+    @Override
+    public void setAdapter(Adapter adapter) {
+        if (!(adapter instanceof SnapRecyclerAdapter)) {
+            throw new IllegalArgumentException(getClass().getSimpleName() + " must be used with a "
+                    + SnapRecyclerAdapter.class.getSimpleName() + " instance.");
+        }
+        super.setAdapter(adapter);
+    }
+}
---
+++ centresnaprecyclerview/src/main/java/com/youview/centresnaprecyclerview/CentreSnapScrollListener.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2015 YouView TV Limited.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.youview.centresnaprecyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * <p>Implementation of a {@link RecyclerView.OnScrollListener} that snaps the central item in the
+ * {@link RecyclerView} to the centre of the view once the scroll has completed.</p>
+ */
+public class CentreSnapScrollListener extends RecyclerView.OnScrollListener {
+    private static final int SNAP_THRESHOLD_PIXELS = 1;
+
+    private int mPreviousScrollState = Integer.MIN_VALUE;
+
+    /**
+     * {@inheritDoc}
+     * <p>Once a scroll has finished (i.e. {@code newState} <i>becomes</i>
+     * {@link RecyclerView#SCROLL_STATE_IDLE}), calculates the delta between the centre of the
+     * RecyclerView and the centre child view, and scrolls by that amount to centre the child.</p>
+     */
+    @Override
+    public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+        if (newState == RecyclerView.SCROLL_STATE_IDLE && newState != mPreviousScrollState) {
+            int centreX = recyclerView.getWidth() / 2;
+            int centreY = recyclerView.getHeight() / 2;
+            View centreChild = recyclerView.findChildViewUnder(centreX, centreY);
+
+            SnapRecyclerAdapter adapter = (SnapRecyclerAdapter) recyclerView.getAdapter();
+            int childPosition = recyclerView.getChildLayoutPosition(centreChild);
+            adapter.onItemSelected(childPosition);
+
+            int left = centreChild.getLeft();
+            int right = centreChild.getRight();
+            int leftFromCentre = centreX - left;
+            int rightFromCentre = right - centreX;
+            int delta = (rightFromCentre - leftFromCentre) / 2;
+
+            // Avoid infinite scrolls where the parity of the screen width and the view width are
+            // different.
+            if (Math.abs(delta) > SNAP_THRESHOLD_PIXELS) {
+                recyclerView.smoothScrollBy(delta, 0);
+            }
+        } else {
+            super.onScrollStateChanged(recyclerView, newState);
+        }
+
+        mPreviousScrollState = newState;
+    }
+}
